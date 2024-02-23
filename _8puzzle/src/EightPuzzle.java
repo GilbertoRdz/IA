c85f3a158df40e9a14ccfd4db21d5ad7807a4919
GilbertoRDZ <l1070805@culiacan.tecnm.mx>
2024-02-22 18:31:55 -0700

    SHF

--- _8puzzle/src/EightPuzzle.java
+++ _8puzzle/src/EightPuzzle.java
@@ -3,30 +3,80 @@ import java.util.*;
 public class EightPuzzle {
     public static void main(String[] args) {
         EightPuzzle puzzleSolver = new EightPuzzle();
+        Scanner scanner = new Scanner(System.in);
         String initialState = "807625341";
-        puzzleSolver.solvePuzzleBFS(initialState);
+
+        System.out.println("Choose one kind of search: ");
+        System.out.println("1.- Breadth First Search");
+        System.out.println("2.- Depth First Search");
+
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                long startTimeBFS = System.currentTimeMillis();
+                puzzleSolver.solvePuzzleBFS(initialState);
+                long endTimeBFS = System.currentTimeMillis();
+                long elapsedTimeBFS = endTimeBFS - startTimeBFS;
+                System.out.println("Breadth First Search Elapsed time: " + elapsedTimeBFS + " milliseconds");
+                break;
+            case 2:
+                long startTimeDFS = System.currentTimeMillis();
+                puzzleSolver.solvePuzzleDFS(initialState);
+                long endTimeDFS = System.currentTimeMillis();
+                long elapsedTimeDFS = endTimeDFS - startTimeDFS;
+                System.out.println("Depth First Search Elapsed time: " + elapsedTimeDFS + " milliseconds");
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
     }
-
     public void solvePuzzleBFS(String initialState) {
-        Queue<Node> openSet = new LinkedList<>();
-        Set<String> closedSet = new HashSet<>();
+        Queue<Node> unknownSet = new LinkedList<>();
+        Set<String> visitedSet = new HashSet<>();
 
-        Node startNode = new Node(initialState, 0, null);
-        openSet.add(startNode);
+        Node startNode = new Node(initialState, null);
+        unknownSet.add(startNode);
 
-        while (!openSet.isEmpty()) {
-            Node current = openSet.poll();
+        while (!unknownSet.isEmpty()) {
+            Node current = unknownSet.poll();
 
             if (isGoalState(current.state)) {
                 printSolution(current);
                 return;
             }
-            closedSet.add(current.state);
+            visitedSet.add(current.state);
             List<String> successors = getSuccessors(current.state);
             for (String successor : successors) {
-                if (!closedSet.contains(successor)) {
-                    Node succesorNode = new Node(successor, 0, current);
-                    openSet.add(succesorNode);
+                if (!visitedSet.contains(successor)) {
+                    Node succesorNode = new Node(successor, current);
+                    unknownSet.add(succesorNode);
+                }
+            }
+        }
+
+        System.out.println("No solution found");
+    }
+
+    public void solvePuzzleDFS(String initialState) {
+        Stack<Node> unknownSet = new Stack<>();
+        Set<String> visitedSet = new HashSet<>();
+
+        Node startNode = new Node(initialState, null);
+        unknownSet.add(startNode);
+
+        while (!unknownSet.isEmpty()) {
+            Node current = unknownSet.pop();
+
+            if (isGoalState(current.state)) {
+                printSolution(current);
+                return;
+            }
+            visitedSet.add(current.state);
+            List<String> successors = getSuccessors(current.state);
+            for (String successor : successors) {
+                if (!visitedSet.contains(successor)) {
+                    Node succesorNode = new Node(successor, current);
+                    unknownSet.add(succesorNode);
                 }
             }
         }
@@ -37,16 +87,16 @@ public class EightPuzzle {
     private List<String> getSuccessors(String state) {
         List<String> successors = new ArrayList<>();
 
-        int spaceIndex = state.indexOf("0");
+        int zeroIndex = state.indexOf("0");
 
         int[] possibleMoves = {-1, 1, -3, 3};
 
         for (int move : possibleMoves) {
-            int successorIndex = spaceIndex + move;
-            if (isValidMove(spaceIndex, successorIndex)) {
+            int successorIndex = zeroIndex + move;
+            if (isValidMove(zeroIndex, successorIndex)) {
                 char[] newStateArray = state.toCharArray();
-                char temp = newStateArray[spaceIndex];
-                newStateArray[spaceIndex] = newStateArray[successorIndex];
+                char temp = newStateArray[zeroIndex];
+                newStateArray[zeroIndex] = newStateArray[successorIndex];
                 newStateArray[successorIndex] = temp;
 
                 successors.add(new String(newStateArray));
@@ -56,10 +106,10 @@ public class EightPuzzle {
         return successors;
     }
 
-    private boolean isValidMove(int spaceIndex, int neighborIndex) {
-        return neighborIndex >= 0 && neighborIndex < 9 &&
-                !(spaceIndex % 3 == 0 && neighborIndex % 3 == 2) &&
-                !(spaceIndex % 3 == 2 && neighborIndex % 3 == 0);
+    private boolean isValidMove(int zeroIndex, int successorIndex) {
+        return successorIndex >= 0 && successorIndex < 9 &&
+                !(zeroIndex % 3 == 0 && successorIndex % 3 == 2) &&
+                !(zeroIndex % 3 == 2 && successorIndex % 3 == 0);
     }
 
     private boolean isGoalState(String state) {
@@ -76,8 +126,21 @@ public class EightPuzzle {
         }
 
         while (!path.isEmpty()) {
-            System.out.println(path.pop());
+            System.out.println(format(path.pop()));
         }
+    }
+
+    private String format(String state) {
+        StringBuilder formatted = new StringBuilder();
+        for (int i = 0; i < 9; i++) {
+            formatted.append(state.charAt(i));
+            if (i % 3 == 2) {
+                formatted.append("\n");
+            } else {
+                formatted.append(" ");
+            }
+        }
+        return formatted.toString();
     }
 }
 
--- _8puzzle/src/Node.java
+++ _8puzzle/src/Node.java
@@ -1,16 +1,10 @@
-public class Node implements Comparable<Node> {
+public class Node  {
     String state;
-    int cost;
     Node parent;
 
-    public Node(String state, int cost, Node parent) {
+    public Node(String state, Node parent) {
         this.state = state;
-        this.cost = cost;
         this.parent = parent;
     }
 
-    @Override
-    public int compareTo(Node other) {
-        return Integer.compare(this.cost, other.cost);
-    }
 }
